9e109b8cdfb32f70f5ece60885d58582b92f5264
ss <setrotec@outlook.com>
2022-06-06 22:34:24 -0300

    Implementando funções

--- monitoramentoweb.go
+++ monitoramentoweb.go
@@ -5,23 +5,45 @@ package main
 import (
 	// formataçao (impressão e formatação de saidas)
 	"fmt"
-
-	/*O pacote reflect implementa a reflexão em tempo de execução,
-	permitindo que um programa manipule objetos com tipos arbitrários
-	*/
+	"os"
 	"reflect"
 )
 
 //função principal
 func main() {
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+	switch comando {
+	case 1:
+		fmt.Println("Monitoramento iniciado")
+	case 2:
+		fmt.Println("Carregando logs")
+	case 3:
+		sairDoPrograma()
+	default:
+		erro()
+	}
+}
+func exibeIntroducao() {
 
 	//oprador:= usado para não declarar a palavra reservada var antes da variavel
 	versao := 1.1
-
 	//reflect.TypeOf retorna o tipo do paramentro
+	fmt.Println("versão", versao)
 	fmt.Println("O tipo da variavel nome é:", reflect.TypeOf(versao))
+}
+func leComando() int {
+	var comandoLido int
+	fmt.Println("Digite um numero para começar")
+	// & =  endereço da variavel que o imput sera salvo
+	fmt.Scanf("%d", &comandoLido)
+	return comandoLido
 
-	//monitoramentoewb
+}
+func exibeMenu() {
+	//menu
 	fmt.Println("========================")
 	fmt.Println("Menu de Opções ")
 	fmt.Println("-------------------------")
@@ -32,24 +54,12 @@ func main() {
 	fmt.Println("3 - Sair do Programa     ")
 	fmt.Println("-------------------------")
 
-	comando := 0
-
-	fmt.Println("Digite um numero para começar")
-	// & =  endereço da variavel que o imput sera salvo
-	fmt.Scanf("%d", &comando)
-
-	if comando == 1 {
-		fmt.Println("Monitoramento iniciado")
-
-	} else if comando == 2 {
-		fmt.Println("Carregando logs")
-
-	} else if comando == 3 {
-		fmt.Println("Saindo do Programa...")
-
-	} else {
-		fmt.Println("Comando digitado diferente das opções listadas")
-
-	}
-
+}
+func sairDoPrograma() {
+	fmt.Println("Programa Finalizado com Sucesso!")
+	os.Exit(0)
+}
+func erro() {
+	fmt.Println("Comando digitado diferente das opções listadas")
+	os.Exit(-1)
 }
