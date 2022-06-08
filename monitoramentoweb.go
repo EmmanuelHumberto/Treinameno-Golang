42cb7f22e1923038c7b3b3d0addfddf06cc1c3cb
ss <setrotec@outlook.com>
2022-06-07 21:20:41 -0300

    Implementando função de ecrita e exibição de logs e finalizando treinamento

--- monitoramentoweb.go
+++ monitoramentoweb.go
@@ -3,34 +3,49 @@ package main
 
 //importando pacote:
 import (
+	//O pacote bufio implementa E/S em buffer.
 	"bufio"
+	//O pacote io fornece interfaces básicas para primitivas de E/S.
+	//Seu trabalho principal é envolver as implementações existentes de tais primitivas,
+	//como aquelas no pacote os
 	"io"
+	//O pacote ioutil implementa algumas funções de utilitário de E/S.
+	"io/ioutil"
+	//O pacote strconv implementa conversões de e para representações de string de tipos de dados básicos.
+	"strconv"
+	//As strings de pacote implementam funções simples para manipular strings codificadas em UTF-8.
 	"strings"
-
 	// formataçao (impressão e formatação de saidas)
 	"fmt"
+	//A rede de pacotes fornece uma interface portátil para E/S de rede, incluindo TCP/IP, UDP,
+	//resolução de nomes de domínio e soquetes de domínio Unix.
 	"net/http"
+	//Pacote os responsavel pela conversa com os sistema opracional
 	"os"
+	//O tempo do pacote fornece funcionalidade para medir e exibir o tempo.
+	//Os cálculos de calendário sempre assumem um calendário gregoriano, sem segundos bissextos.
 	"time"
 )
 
-const daley = 60
+const daley = 5
 
 //função principal
 func main() {
 	exibeIntroducao()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	exibeMenu()
-	comando := leComando()
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Carregando logs")
-	case 3:
-		sairDoPrograma()
-	default:
-		erro()
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			exibeLogs()
+		case 3:
+			sairDoPrograma()
+		default:
+			erro()
+		}
 	}
 }
 func exibeIntroducao() {
@@ -90,16 +105,16 @@ func iniciarMonitoramento() {
 }
 func testaSite(site string) {
 	resp, err := http.Get(site)
-
 	if err != nil {
 		fmt.Println("Ocorreu um erro", err)
 	}
-
 	if resp.StatusCode == 200 {
 		fmt.Println("Sites: carregados com sucesso")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Sites: problemas ao carregar. ", "Status Code",
 			resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 func leSitesArquivo() []string {
@@ -128,4 +143,30 @@ func leSitesArquivo() []string {
 	//Fechando arquivo apos a leitura
 	arquivoSite.Close()
 	return sites
+}
+func registraLog(site string, status bool) {
+	//os.OpenFile( O_RDWR = abre o arquivo para leitura e escrita
+	//O_CREATE cria o arquivo caso ele não exista. 0666 permissão para leitura e escrita
+	//O_APPEND: anexa dados ao arquivo durante a escrita.
+	arquivoLog, err := os.OpenFile("listaLog.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
+	//WriteString é como Write, mas grava o conteúdo da string s em vez de uma fatia de bytes.
+	//strconv.FormatBool: converte de bool para string
+	//time.Now().Format();Format retorna uma representação textual do valor de tempo
+	//formatado de acordo com o layout definido pelo argumento
+	arquivoLog.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - Online: " +
+		strconv.FormatBool(status) + "\n")
+	arquivoLog.Close()
+}
+func exibeLogs() {
+	//O pacote ioutil implementa algumas funções de utilitário de E/S.
+	//ReadFile lê o arquivo nomeado pelo nome do arquivo e retorna o conteúdo
+	arquivoExibeLog, err := ioutil.ReadFile("listaLog.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	} else {
+		fmt.Println(string(arquivoExibeLog))
+	}
 }
